cb6bd8e03fb0e4dc7e24bcc5911c19292d371835
jfrancis01 <joelfrancis@rogers.com>
2023-11-13 22:01:39 -0500

    fixed edit and new routing

--- src/app/recipebook/recipe/edit-recipe/edit-recipe.component.ts
+++ src/app/recipebook/recipe/edit-recipe/edit-recipe.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-edit-recipe',
   templateUrl: './edit-recipe.component.html',
   styleUrls: ['./edit-recipe.component.css']
 })
-export class EditRecipeComponent {
+export class EditRecipeComponent implements OnInit{
 
+  edit:boolean = false;
+  index: number;
+
+  constructor(private route: ActivatedRoute){
+
+  }
+
+  ngOnInit(): void {
+      this.route.params.subscribe((params: Params) => {
+        this.index = +params['index'];
+        this.edit = params['index'] != null;
+        console.log("Edit mode: " + this.edit);
+      });
+  }
 }
